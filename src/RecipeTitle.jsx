211de530226acd8f3d17f4c8dca85133fdc03500
adog <adrianmg3008@gmail.com>
2025-05-20 11:34:15 -0400

    Refactor styles for IngredientList, InstructionList, RecipeTitle, and index components to enhance layout and visual consistency

--- src/RecipeTitle.jsx
+++ src/RecipeTitle.jsx
@@ -3,7 +3,7 @@ import "./RecipeTitle.css";
 
 function RecipeTitle(props) {
   return (
-    <section>
+    <section className="recipe-header">
       <h2>{props.title}</h2>
       <h3 className={props.feedback.rating <= 3.5 ? "red" : "green"}>
         {props.feedback.rating} from {props.feedback.reviews} reviews
